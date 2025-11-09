e9dc86394e769040b0592249ea45922f3dd88f8b
Thoru213 <herdinthorikn@gmail.com>
2025-11-09 20:20:54 +0700

    Changing icon and sidebar in admin site

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,11 +1,11 @@
 <div class="sidebar">
     <ul>
-        <li><a href="{{ route('admin.dashboard') }}">🏠 Dashboard</a></li>
-        <li><a href="{{ route('admin.pertanian.index') }}">🌾 Pertanian & Perkebunan</a></li>
-        <li><a href="{{ route('admin.pariwisata.index') }}">🏕️ Pariwisata</a></li>
-        <li><a href="{{ route('admin.umkm.index') }}">🧺 UMKM</a></li>
-        <li><a href="{{ route('admin.galeri.index') }}">🖼️ Galeri</a></li>
-        <li><a href="{{ route('admin.settings.tentang-kami') }}">📜 Tentang Kami</a></li>
-        <li><a href="{{ route('admin.settings.kontak') }}">📍 Kontak</a></li>
+        <li><a href="{{ route('admin.dashboard') }}"><i class="fas fa-home"></i> Dashboard</a></li>
+        <li><a href="{{ route('admin.pertanian.index') }}"><i class="fas fa-seedling"></i> Pertanian & Perkebunan</a></li>
+        <li><a href="{{ route('admin.pariwisata.index') }}"><i class="fas fa-mountain"></i> Pariwisata</a></li>
+        <li><a href="{{ route('admin.umkm.index') }}"><i class="fas fa-store"></i> UMKM</a></li>
+        <li><a href="{{ route('admin.galeri.index') }}"><i class="fas fa-images"></i> Galeri</a></li>
+        <li><a href="{{ route('admin.settings.tentang-kami') }}"><i class="fas fa-info-circle"></i> Tentang Kami</a></li>
+        <li><a href="{{ route('admin.settings.kontak') }}"><i class="fas fa-map-marker-alt"></i> Kontak</a></li>
     </ul>
 </div>
